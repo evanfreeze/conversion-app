7be446d93e27ffe55e1fdfc717c88e4c28432557
Evan Freeze <evanfreeze@gmail.com>
2020-11-22 19:21:46 -0600

    Initial Commit

---
+++ Conversion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Conversion
+//
+//  Created by Evan Freeze on 11/22/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Conversion/ConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConversionApp.swift
+//  Conversion
+//
+//  Created by Evan Freeze on 11/22/20.
+//
+
+import SwiftUI
+
+@main
+struct ConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
